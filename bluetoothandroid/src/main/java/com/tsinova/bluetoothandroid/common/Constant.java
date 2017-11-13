ca592a7ec9029718a8b24409830e0a8cccc392ea
warry19940712 <418340471@qq.com>
2017-11-13 16:29:45 +0800

    打开log

--- bluetoothandroid/src/main/java/com/tsinova/bluetoothandroid/common/Constant.java
+++ bluetoothandroid/src/main/java/com/tsinova/bluetoothandroid/common/Constant.java
@@ -7,7 +7,7 @@ package com.tsinova.bluetoothandroid.common;
 public class Constant {
 
     /** 是否显示日志--仅供开发使用 推送也用的这个*/
-    public static boolean isShowLog = false; // true开启log，false关闭log
+    public static boolean isShowLog = true; // true开启log，false关闭log
     /** 日志TAG */
     public static final String LOG_TAG = "Tsinova BlueTooth SDK";
 
