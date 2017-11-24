8162e0e7d336926f1f6a6b0eee59fb55df286b89
DESKTOP-IJALPPN\ihgoo <ihgoo2008@gmail.com>
2017-11-24 16:16:17 +0800

    send lt

--- bluetoothandroid/src/main/java/com/tsinova/bluetoothandroid/pojo/BlueToothRequstInfo.java
+++ bluetoothandroid/src/main/java/com/tsinova/bluetoothandroid/pojo/BlueToothRequstInfo.java
@@ -9,23 +9,11 @@ public class BlueToothRequstInfo implements Serializable {
 
     private String lt; // 此值为1,代表前灯打开,此值为0,代表前灯关闭。
 
-    private String st; // 此值为1,代表车辆正在行行驶,此值为0,代表车辆停止止运行。
-
-    private String ge; // 当前挡位。此值为 1,代表高档,此值为 0,代表低档。
-
-    private String md; // 此值为"0",代表正常模式;此值为"1",代表快速启动模式。
-
-    private String ve; // 此值为当前蓝⽛牙固件版本号,为两位。
-
     public BlueToothRequstInfo() {
     }
 
     public BlueToothRequstInfo(String lt, String st, String ge, String md, String ve) {
         this.lt = lt;
-        this.st = st;
-        this.ge = ge;
-        this.md = md;
-        this.ve = ve;
     }
 
     public String getLt() {
@@ -34,38 +22,6 @@ public class BlueToothRequstInfo implements Serializable {
 
     public void setLt(String lt) {
         this.lt = lt;
-    }
-
-    public String getSt() {
-        return st;
-    }
-
-    public void setSt(String st) {
-        this.st = st;
-    }
-
-    public String getGe() {
-        return ge;
-    }
-
-    public void setGe(String ge) {
-        this.ge = ge;
-    }
-
-    public String getMd() {
-        return md;
-    }
-
-    public void setMd(String md) {
-        this.md = md;
-    }
-
-    public String getVe() {
-        return ve;
-    }
-
-    public void setVe(String ve) {
-        this.ve = ve;
     }
 
 
