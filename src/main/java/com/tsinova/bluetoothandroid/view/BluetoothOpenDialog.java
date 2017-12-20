6cd506c57ca62e0024442a4132b5e50e39c035bd
warry19940712 <418340471@qq.com>
2017-12-20 14:57:13 +0800

    更新文案，更新SDK

--- src/main/java/com/tsinova/bluetoothandroid/view/BluetoothOpenDialog.java
+++ src/main/java/com/tsinova/bluetoothandroid/view/BluetoothOpenDialog.java
@@ -1,23 +1,21 @@
 package com.tsinova.bluetoothandroid.view;
 
 import android.app.Activity;
-import android.app.Dialog;
 import android.content.DialogInterface;
 
 import com.tsinova.bluetoothandroid.R;
 import com.tsinova.bluetoothandroid.util.UIUtils;
 
 /**
- * Created by xucong on 2017/12/19.
+ * Created by xucong on 2017/12/20.
  */
 
 public class BluetoothOpenDialog {
-    public static void creatBluetoothOpenDialog(final Activity activity) {
-        String title = activity.getResources().getString(R.string.sdk_bltmanager_search_bl_dialog_tip);
-        String content = activity.getResources().getString(R.string.sdk_bltmanager_search_bl_dialog_open_ble);
-        String ok = activity.getResources().getString(R.string.sdk_bltmanager_search_bl_dialog_ok);
-        UIUtils.createSigleBtnDialog(activity, title, content, ok, new Dialog.OnClickListener() {
-            @Override
+    public static void creatBluetoothOpenDialog(Activity activity) {
+        String title =  activity.getString(com.tsinova.bluetoothandroid.R.string.sdk_bltmanager_search_bl_dialog_tip);
+        String content = activity.getString(com.tsinova.bluetoothandroid.R.string.sdk_bltmanager_search_bl_dialog_open_ble);
+        String ok = activity.getString(com.tsinova.bluetoothandroid.R.string.sdk_bltmanager_search_bl_dialog_ok);
+        UIUtils.createSigleBtnDialog(activity, title, content, ok, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {
                 dialog.dismiss();
             }
